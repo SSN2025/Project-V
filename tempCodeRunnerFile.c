a8f4fe32438ee96626edc821748ba0e247f79f55
SSN2025 <suyashnaik2807@gmail.com>
2025-12-29 17:00:17 +0530

    Initial commit

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+}
+
+// int valid_obstacle_wise(int brd[8][8],int c_fr, int r_fr, int c_to, int r_to){
+// if(brd[r_fr][c_fr] *brd[r_to][c_to]  <=0){
+//     return 1;
+// }
+// else{
+//     return 0;
+// }
+// }
